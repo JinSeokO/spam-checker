f8593456febe4170d72d8c3d5a0d4dab06beff16
오진석 <freejin4802@gmail.com>
2021-12-27 14:25:19 +0900

    add hasUrl method

--- internal/service/spam.go
+++ internal/service/spam.go
@@ -1,9 +1,48 @@
 package service
 
+import (
+	"log"
+	"net/url"
+	"strings"
+)
+
 type Spam struct {
 }
 
 func (s Spam) IsSpam(contents string, spamLinkDomains []string, redirectionDepth int) (bool, error) {
-	//TODO implement me
-	panic("implement me")
+	link, ok := hasURL(contents)
+	if ok != true {
+		return false, nil
+	}
+
+	log.Println(link)
+
+	return false, nil
+}
+
+func hasURL(contents string) (url string, ok bool) {
+	fields := strings.Fields(contents)
+
+	for _, field := range fields {
+		if isURL(field) {
+			return field, true
+		}
+	}
+
+	return "", false
+}
+
+func isURL(urlSample string) bool {
+	_, err := url.ParseRequestURI(urlSample)
+	if err != nil {
+		return false
+	}
+
+	u, err := url.Parse(urlSample)
+
+	if err != nil || u.Host == "" || u.Scheme == "" {
+		return false
+	}
+
+	return true
 }
--- internal/service/spam_test.go
+++ internal/service/spam_test.go
@@ -1,25 +1,66 @@
 package service
 
 import (
-	"fmt"
 	"github.com/stretchr/testify/assert"
+	"log"
+	"strings"
 	"testing"
 )
 
-var spamService = Spam{}
+//var spamService = Spam{}
 
 func TestSpam_isSpam(t *testing.T) {
+	//type args struct {
+	//	contents         string
+	//	spamLinkDomains  []string
+	//	redirectionDepth int
+	//}
+	//assert.Equal(t, false, true, "It should not be equal")
+	log.Println(strings.Fields("test result expect"))
+	//testCases := []args{
+	//	{
+	//		contents:         fmt.Sprintf("spam spam https://goo.gl/nVLutc"),
+	//		spamLinkDomains:  []string{"http://www.filekok.com/main"},
+	//		redirectionDepth: 1,
+	//	},
+	//}
+}
+
+func Test_hasURL(t *testing.T) {
 	type args struct {
-		contents         string
-		spamLinkDomains  []string
-		redirectionDepth int
+		contents string
 	}
-	assert.Equal(t, false, true, "It should not be equal")
-	testCases := []args{
+	tests := []struct {
+		name    string
+		args    args
+		wantUrl string
+		wantOk  bool
+	}{
 		{
-			contents:         fmt.Sprintf("spam spam https://goo.gl/nVLutc"),
-			spamLinkDomains:  []string{"http://www.filekok.com/main"},
-			redirectionDepth: 1,
+			name:    "default url",
+			args:    args{contents: "has url https://www.google.com"},
+			wantUrl: "https://www.google.com",
+			wantOk:  true,
 		},
+		{
+			name:    "has path param url",
+			args:    args{contents: "has url https://www.google.com/test00"},
+			wantUrl: "https://www.google.com/test00",
+			wantOk:  true,
+		},
+		{
+			name:    "no url case",
+			args:    args{contents: "has url google"},
+			wantUrl: "",
+			wantOk:  false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotUrl, gotOk := hasURL(tt.args.contents)
+			assert.Equal(t, tt.wantOk, gotOk)
+			assert.Equal(t, tt.wantUrl, gotUrl)
+		})
 	}
 }
